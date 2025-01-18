5aedcb6513ab1eb75a52641f1e6863bb459a4f1b
kai-1208 <tkhski1208@gmail.com>
2025-01-19 01:59:14 +0900

    コース画像のインデックスを修正し、正しい画像を表示するように変更

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ class TimeAttack:
         self.x_pos, self.y_pos, self.rot = 0, 0, 0
         self.frame = np.random.uniform(0, 1, (self.hres, self.harf_vres*2, 3))
 
-        self.cource = pg.surfarray.array3d(self.resources.cource_images[3]["show"])
+        self.cource = pg.surfarray.array3d(self.resources.cource_images[0]["show"])
         self.sky = pg.surfarray.array3d(pg.transform.scale(self.resources.cource_images[0]["sky"], (360, self.harf_vres*2)))
         # self.game_state = "running"
 
